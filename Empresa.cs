8745a0fad3aca66467fd3b3e7edd4ccb363de189
EQUIPO-LABB04 <EQUIPO-LABB04@EQUIPO-LABB10>
2023-05-09 09:35:38 -0600

    Agregar archivos de proyecto.

---
+++ Empresa.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+class Empresa
+{
+    public int ID;
+    public string ApellidoP;
+    public string ApellidoM;
+    public string Nombre;
+    public DateTime FechaI;
+}
+
+class PersoanalContratado : Empresa
+{
+    public int añoslaborados;
+    public double porcentajeadicional;
+
+    public PersonalContratado(int id, string apellidoPaterno, string apellidoMaterno, string nombre, DateTime fechaIngreso, double salario, int añosLaborados)
+    {
+        this.ID = id;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.nombre = nombre;
+        this.fechaIngreso = fechaIngreso;
+        this.añoslaborados = añosLaborados;
+
+        if (añosLaborados >= 0 && añosLaborados <= 3)
+        {
+            this.porcentajeadicional = 0.05;
+        }
+        else if (añosLaborados >= 4 && añosLaborados <= 7)
+        {
+            this.porcentajeadicional = 0.10;
+        }
+        else if (añosLaborados >= 8 && añosLaborados <= 15)
+        {
+            this.porcentajeadicional = 0.15;
+        }
+        else if (añosLaborados > 15)
+        {
+            this.porcentajeadicional = 0.20;
+        }
+    }
+}
+
+class PersonalAdestajo : Empresa
+{
+    public int clientesCaptados;
+
+    public PersonalAdestajo(int id, string apellidoPaterno, string apellidoMaterno, string nombre, DateTime fechaIngreso, double salario, int clientesCaptados)
+    {
+
+        this.ID = id;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.Nombre = nombre;
+        this.fechaIngreso = fechaIngreso;
+        this.salario = salario;
+        this.clientesCapatados = clientesCaptados;
+    }
+
+    public void CalcularSalario()
+    {
+        this.salario = this.salario + (this.clientesCaptados * 130.0);
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static List<PersonalContratado> personalContratadoList = new List<PersonalContratado>();
+    static List<PersonalAdestajo> personalAdestajoList = new List<PersonalAdestajo>();
+
+    static void Main(string[] args)
+    {
+        int opcion = 0;
+
+        do
+        {
+            Console.WriteLine("Bienvenidos al sistema de administración de persoanl");
+            Console.WriteLine("Seleccione una opción del menú");
+            Console.WriteLine("1. Registrar empleado contratado:");
+            Console.WriteLine("2. Registrar empleados a destajo");
+            Console.WriteLine("3. Mostrar salarios de los empleados");
+            Console.WriteLine("4. Salir");
+
+            try
+            {
+                opcion = int.Parse(Console.ReadLine());
+
+                switch
+            }
+
+
+    }
+}
